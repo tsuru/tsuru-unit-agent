0486e450383b83a4812fb230455eac4e775572f3
Andrews Medina <andrewsmedina@gmail.com>
2014-03-17 23:58:49 -0300

    added setup.

---
+++ setup.py
@@ -0,0 +1,18 @@
+# Copyright 2014 tsuru-unit-agent authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+from setuptools import setup, find_packages
+from tsuru_unit_agent import __version__
+
+
+setup(
+    name="tsuru-unit-agent",
+    version=__version__,
+    packages=find_packages(),
+    description="Tsuru unit agent.",
+    author="tsuru",
+    author_email="tsuru@corp.globo.com",
+    include_package_data=True,
+    install_requires=["requests"],
+)
