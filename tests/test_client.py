7a3ded41ebb656e3971d4aca0745f39b12e2ccb2
Andrews Medina <andrewsmedina@gmail.com>
2014-03-18 01:01:41 -0300

    implemented get_envs.

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,5 @@
 import unittest
+import mock
 
 from tsuru_unit_agent.client import Client
 
@@ -7,3 +8,12 @@ class TestClient(unittest.TestCase):
     def test_client(self):
         client = Client(url="http://localhost")
         self.assertEqual(client.url, "http://localhost")
+
+    @mock.patch("requests.get")
+    def test_get_envs(self, get_mock):
+        get_mock.return_value = "{}"
+        client = Client(url="http://localhost")
+        envs = client.get_envs(app="myapp")
+        self.assertDictEqual(envs, {})
+        get_mock.assert_called_with(
+            "{}/apps/myapp/envs".format(client.url))
--- tsuru_unit_agent/client.py
+++ tsuru_unit_agent/client.py
@@ -1,3 +1,11 @@
+import requests
+import json
+
+
 class Client(object):
     def __init__(self, url):
         self.url = url
+
+    def get_envs(self, app):
+        response = requests.get("{}/apps/{}/envs".format(self.url, app))
+        return json.loads(response)
