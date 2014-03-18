217776dca438b91444ed690dfa1083ab264f0e64
Andrews Medina <andrewsmedina@gmail.com>
2014-03-18 00:36:28 -0300

    tasks: implemented save_apprc_file.

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,4 +1,4 @@
-from tsuru_unit_agent.tasks import execute_start_script
+from tsuru_unit_agent.tasks import execute_start_script, save_apprc_file
 
 import unittest
 import mock
@@ -10,3 +10,16 @@ class TestTasks(unittest.TestCase):
     def test_execute(self, call_mock):
         execute_start_script()
         call_mock.assert_called_with(["/var/lib/tsuru/start"])
+
+    @mock.patch("io.open")
+    def test_save_apprc_file(self, open_mock):
+        file_mock = open_mock.return_value
+        environs = {
+            "DATABASE_HOST": "localhost",
+            "DATABASE_USER": "root",
+        }
+
+        save_apprc_file(environs)
+
+        open_mock.assert_called_with("/home/application/apprc")
+        file_mock.write.assert_called()
