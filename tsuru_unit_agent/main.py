c91f7780dd9ceed9808d9cddbee22d8503237165
Andrews Medina <andrewsmedina@gmail.com>
2014-03-20 02:39:40 -0300

    main: getting variables from environ.

--- tsuru_unit_agent/main.py
+++ tsuru_unit_agent/main.py
@@ -1,9 +1,14 @@
+import os
+
 from tsuru_unit_agent import tasks
 from tsuru_unit_agent.client import Client
 
 
 def main():
-    client = Client("http://localhost", "token")
-    envs = client.get_envs("app_name")
+    token = os.environ.get("TSURU_TOKEN")
+    url = os.environ.get("TSURU_URL")
+    app_name = os.environ.get("APPNAME")
+    client = Client(url, token)
+    envs = client.get_envs(app_name)
     tasks.save_apprc_file(envs)
     tasks.execute_start_script()
