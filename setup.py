fc5037437017a07b410b460cca7b4ba7ea6237e0
Cezar Sa Espinola <cezar.sa@corp.globo.com>
2015-01-09 16:43:43 -0200

    honcho master when released may break us

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author="tsuru",
     author_email="tsuru@corp.globo.com",
     include_package_data=True,
-    install_requires=["requests", "PyYAML", "honcho"],
+    install_requires=["requests", "PyYAML", "honcho==0.5.0"],
     entry_points={
         'console_scripts': [
             'tsuru_unit_agent = tsuru_unit_agent.main:main',
